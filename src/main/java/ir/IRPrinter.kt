96c995aa6474f128f5776ffa1b3d058499c27c2d
Brian Chen <ToucheSir@users.noreply.github.com>
2018-03-12 09:17:36 -0700

    IR printing fixes

--- src/main/java/ir/IRPrinter.kt
+++ src/main/java/ir/IRPrinter.kt
@@ -1,10 +1,13 @@
 package ir
 
+import org.apache.commons.text.StringEscapeUtils.escapeJava
 import type.*
 import java.io.PrintStream
 
 class IRPrinter(val out: PrintStream) {
     fun print(program: IRProgram) {
+        // TODO pass in program name from IR Node
+        out.println("PROG ULMain")
         program.functions.forEach(this::print)
     }
 
@@ -15,27 +18,40 @@ class IRPrinter(val out: PrintStream) {
         out.print(')')
         print(sig.returnType)
         out.println("\n{")
-        func.temporaries.forEach(this::print)
+        func.temporaries.params.forEach { print(it, "P") }
+        func.temporaries.locals.forEach { print(it) }
+        func.temporaries.temps.forEach { print(it) }
         func.instructions.forEach(this::print)
         out.println('}')
     }
 
-    fun print(temporary: Temporary) {
+    fun print(temporary: Temporary, annotationType: String = "L") {
         out.print("  TEMP ${temporary.id}:")
         print(temporary.type)
-        temporary.name?.run { out.print(" [L(\"$this\")]") }
+        temporary.name?.run { out.print(" [$annotationType(\"$this\")]") }
         out.println(';')
     }
 
     fun print(instruction: IRInstruction) {
         out.print("  ")
         when (instruction) {
-            is IRLoadImmediate<*> -> out.print("${instruction.tmp} := ${instruction.value}")
+            is IRLoadImmediate<*> -> {
+                out.print("${instruction.tmp} := ")
+                out.print(when (instruction.value) {
+                    is Boolean -> instruction.value.toString().toUpperCase()
+                    is String -> "\"${escapeJava(instruction.value)}\""
+                    is Char -> {
+                        val escaped = escapeJava(instruction.value.toString())
+                        "'${escaped.removeSurrounding("\"")}'"
+                    }
+                    else -> instruction.value
+                })
+            }
             is IRInitArray -> {
                 val (elemType, size) = instruction.tmp.type as ArrayType
-                out.print("${instruction.tmp} := NEWARRAY (")
+                out.print("${instruction.tmp} := NEWARRAY ")
                 print(elemType)
-                out.print(") $size")
+                out.print(" $size")
             }
             is IRFunctionCall -> {
                 instruction.retVal?.run { out.print("$this := ") }
@@ -44,17 +60,18 @@ class IRPrinter(val out: PrintStream) {
             is IRArrayReference -> out.print("${instruction.res} := ${instruction.arr}[${instruction.index}]")
             is IRBinaryOp -> {
                 out.print("${instruction.res} := ${instruction.lhs} ")
-                print(instruction.res.type)
+                print(instruction.lhs.type)
                 out.print("${instruction.operator.symbol} ${instruction.rhs}")
             }
             is IRUnaryOp -> {
                 out.print("${instruction.res} := ")
-                print(instruction.res.type)
+                print(instruction.operand.type)
                 out.print("${instruction.operator.symbol} ${instruction.operand}")
             }
             is IRAssignment -> {
-                out.print("${instruction.dest} := ${instruction.src}")
+                out.print(instruction.dest)
                 instruction.index?.run { out.print("[$this]") }
+                out.print(" := ${instruction.src}")
             }
             is IRReturn -> {
                 out.print("RETURN")
@@ -62,7 +79,7 @@ class IRPrinter(val out: PrintStream) {
             }
             is IRLabel -> out.print("L${instruction.id}:")
             is IRJump -> out.print("GOTO L${instruction.label.id}")
-            is IRConditionalJump -> out.print("IF ${instruction.cond} GOTO ${instruction.label.id}")
+            is IRConditionalJump -> out.print("IF ${instruction.cond} GOTO L${instruction.label.id}")
             is IRPrint -> {
                 out.print("PRINT")
                 print(instruction.type)
